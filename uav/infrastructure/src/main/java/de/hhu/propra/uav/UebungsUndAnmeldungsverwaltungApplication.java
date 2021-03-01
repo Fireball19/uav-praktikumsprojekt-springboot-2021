c2a010380035c96afe1852e77707d1d4cc7942fa
zandatzu <johannes3097@web.de>
2021-03-01 11:32:04 +0100

    Grundgeruest Onion-Architecture

---
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/UebungsUndAnmeldungsverwaltungApplication.java
@@ -0,0 +1,13 @@
+package de.hhu.propra.uav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebungsUndAnmeldungsverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UebungsUndAnmeldungsverwaltungApplication.class, args);
+	}
+
+}
---
+++ uav/infrastructure/src/test/java/de/hhu/propra/uav/UebungsUndAnmeldungsverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.propra.uav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebungsUndAnmeldungsverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
