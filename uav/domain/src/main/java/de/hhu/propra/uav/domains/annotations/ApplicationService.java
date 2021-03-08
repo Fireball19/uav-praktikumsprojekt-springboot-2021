d870b70061c6df13ce3566b50a3032ac5d26e25e
Fireball19 <fireball1199@googlemail.com>
2021-03-08 12:44:58 +0100

    Added applicationservice annotation

---
+++ uav/domain/src/main/java/de/hhu/propra/uav/domains/annotations/ApplicationService.java
@@ -0,0 +1,8 @@
+package de.hhu.propra.uav.domains.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApplicationService {
+}
--- uav/infrastructure/src/main/java/de/hhu/propra/uav/UebungsUndAnmeldungsverwaltungApplication.java
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/UebungsUndAnmeldungsverwaltungApplication.java
@@ -1,10 +1,19 @@
 package de.hhu.propra.uav;
 
+import de.hhu.propra.uav.domains.annotations.ApplicationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import static org.springframework.context.annotation.FilterType.ANNOTATION;
 
 @SuppressWarnings("PMD")
 @SpringBootApplication
+@ComponentScan(
+        includeFilters = {
+                @ComponentScan.Filter(type = ANNOTATION, classes = ApplicationService.class)
+        }
+)
 public class UebungsUndAnmeldungsverwaltungApplication {
 
   public static void main(String[] args) throws Exception {
