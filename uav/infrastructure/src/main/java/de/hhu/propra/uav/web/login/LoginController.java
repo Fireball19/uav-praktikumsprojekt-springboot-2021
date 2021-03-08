cde0b4092814f292240167f903ced27d5c25c597
Fireball19 <fireball1199@googlemail.com>
2021-03-08 11:12:10 +0100

    Added OAuth2User to controller

--- uav/infrastructure/src/main/java/de/hhu/propra/uav/web/login/LoginController.java
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/web/login/LoginController.java
@@ -1,13 +1,20 @@
 package de.hhu.propra.uav.web.login;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/")
-    public String login() {
-        return "login";
-    }
+  @GetMapping("/")
+  public String index(@AuthenticationPrincipal OAuth2User principal, Model model) {
+    model.addAttribute("user",
+        principal != null ?
+            principal.getAttribute("login") : null
+    );
+    return "login";
+  }
 }
