41e8fb760239e24ec05780bd1d11ae66a3cac547
Fireball19 <fireball1199@googlemail.com>
2021-03-09 12:32:43 +0100

    Added spring security configuration and roles

---
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/configuration/MethodSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package de.hhu.propra.uav.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+
+    // Ermöglicht Annotation von Methoden mit @Secured
+}
