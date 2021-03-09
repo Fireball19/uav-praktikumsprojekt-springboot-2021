09f80a9022b3894ff4eda30d847ae6ca4dbc4215
Fireball19 <fireball1199@googlemail.com>
2021-03-09 12:30:18 +0100

    Fixed package names and ignored applicationservice annotation

--- uav/infrastructure/src/test/java/de/hhu/propra/uav/archunit/ArchUnitTests.java
+++ uav/infrastructure/src/test/java/de/hhu/propra/uav/archunit/ArchUnitTests.java
@@ -4,6 +4,9 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import de.hhu.propra.uav.UebungsUndAnmeldungsverwaltungApplication;
+import de.hhu.propra.uav.authorization.AuthorityService;
+import de.hhu.propra.uav.configuration.WebSecurityConfiguration;
+import de.hhu.propra.uav.domains.annotations.ApplicationService;
 
 import static com.tngtech.archunit.library.Architectures.onionArchitecture;
 
@@ -12,10 +15,15 @@ public class ArchUnitTests {
 
   @ArchTest
   final static ArchRule onionArchitecture = onionArchitecture()
-      .domainModels("de.hhu.propra.uav.domains..")
-      .domainServices("de.hhu.propra.uav.services..")
+      .domainModels("de.hhu.propra.uav.domains.model..")
+      .domainServices("de.hhu.propra.uav.domains.services..")
       .applicationServices("de.hhu.propra.uav.domains..")
-      .adapter("authorization","de.hhu.propra.uav.authorization")
-      .adapter("persistence","de.hhu.propra.uav.persistence")
-      .adapter("web","de.hhu.propra.uav.web");
+      .adapter("authorization", "de.hhu.propra.uav.authorization..")
+      .adapter("persistence", "de.hhu.propra.uav.repositories..")
+      .adapter("web", "de.hhu.propra.uav.web..")
+      // Nice ArchUnit
+      .ignoreDependency(ArchUnitTests.class, AuthorityService.class)
+      .ignoreDependency(ArchUnitTests.class, ApplicationService.class)
+      .ignoreDependency(WebSecurityConfiguration.class, AuthorityService.class)
+      .ignoreDependency(UebungsUndAnmeldungsverwaltungApplication.class, ApplicationService.class);
 }
