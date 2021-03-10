79344a074925054602d1abebc56a09b285c4e6f1
ToxicBill <Michel144@gmx.de>
2021-03-10 10:58:11 +0100

    added Annotation for DomainService

--- uav/infrastructure/src/main/java/de/hhu/propra/uav/UebungsUndAnmeldungsverwaltungApplication.java
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/UebungsUndAnmeldungsverwaltungApplication.java
@@ -1,6 +1,7 @@
 package de.hhu.propra.uav;
 
 import de.hhu.propra.uav.domains.annotations.ApplicationService;
+import de.hhu.propra.uav.domains.annotations.DomainService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,9 +11,10 @@ import static org.springframework.context.annotation.FilterType.ANNOTATION;
 @SuppressWarnings("PMD")
 @SpringBootApplication
 @ComponentScan(
-        includeFilters = {
-                @ComponentScan.Filter(type = ANNOTATION, classes = ApplicationService.class)
-        }
+    includeFilters = {
+        @ComponentScan.Filter(type = ANNOTATION, classes = ApplicationService.class),
+        @ComponentScan.Filter(type = ANNOTATION, classes = DomainService.class)
+    }
 )
 public class UebungsUndAnmeldungsverwaltungApplication {
 
