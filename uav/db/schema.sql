c6a2d2f945e4985148aff2a93952e4a7f8bbc0d5
ToxicBill <Michel144@gmx.de>
2021-03-10 10:58:38 +0100

    added field gruppenname

--- uav/db/schema.sql
+++ uav/db/schema.sql
@@ -14,37 +14,38 @@ CREATE TABLE IF NOT EXISTS uebung
     max_groesse    INTEGER,
     anmeldebeginn  DATETIME,
     anmeldeschluss DATETIME,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS termin
 (
-  id          BIGINT AUTO_INCREMENT,
-  zeitpunkt   DATETIME,
-  reserviert  BOOLEAN,
-  min_groesse INTEGER,
-  max_groesse INTEGER,
-  tutor       VARCHAR(50),
-  uebung_key  BIGINT,
-  uebung      BIGINT,
-  PRIMARY KEY (id),
-  CONSTRAINT fk_termin_uebung FOREIGN KEY (uebung) REFERENCES uebung (id)
+    id          BIGINT AUTO_INCREMENT,
+    zeitpunkt   DATETIME,
+    reserviert  BOOLEAN,
+    min_groesse INTEGER,
+    max_groesse INTEGER,
+    tutor       VARCHAR(50),
+    uebung_key  BIGINT,
+    uebung      BIGINT,
+    gruppenname VARCHAR(50),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_termin_uebung FOREIGN KEY (uebung) REFERENCES uebung (id)
 );
 
 CREATE TABLE IF NOT EXISTS student
 (
-    id BIGINT AUTO_INCREMENT,
+    id     BIGINT AUTO_INCREMENT,
     github VARCHAR(50),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS termin_student
 (
-    termin_key BIGINT,
+    termin_key  BIGINT,
     student_key BIGINT,
-    termin      BIGINT ,
+    termin      BIGINT,
     id          BIGINT,
     PRIMARY KEY (termin, id),
     CONSTRAINT fk_termin_student FOREIGN KEY (id) REFERENCES student (id),
-    CONSTRAINT fk_student_termin FOREIGN KEY (termin) REFERENCES termin(id)
+    CONSTRAINT fk_student_termin FOREIGN KEY (termin) REFERENCES termin (id)
 );
