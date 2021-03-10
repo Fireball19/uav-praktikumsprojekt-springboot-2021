7247942b89191b89323a8b722c7f0ff4bba9d0c8
zandatzu <johannes3097@web.de>
2021-03-10 13:09:42 +0100

    added AnmeldungService

---
+++ uav/domain/src/main/java/de/hhu/propra/uav/domains/services/AnmeldungService.java
@@ -0,0 +1,27 @@
+package de.hhu.propra.uav.domains.services;
+
+import de.hhu.propra.uav.domains.annotations.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ApplicationService
+public class AnmeldungService {
+
+
+  private final UebungService uebungService;
+
+  private final VerwaltungService verwaltungService;
+
+  public AnmeldungService(UebungService uebungService, VerwaltungService verwaltungService) {
+    this.uebungService = uebungService;
+    this.verwaltungService = verwaltungService;
+  }
+
+  public void gruppenAnmeldung(final Long uebungId, final Long terminId,
+                               final String gruppenname, final String mitglieder) {
+    uebungService.addGruppe(uebungId, terminId, gruppenname);
+    final String[] split = mitglieder.split(",");
+    for (String s : split) {
+      verwaltungService.addStudent(s, uebungId, terminId);
+    }
+  }
+}
--- uav/infrastructure/src/main/java/de/hhu/propra/uav/web/anmeldung/AnmeldungController.java
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/web/anmeldung/AnmeldungController.java
@@ -1,17 +1,24 @@
 package de.hhu.propra.uav.web.anmeldung;
 
+import de.hhu.propra.uav.domains.services.AnmeldungService;
 import de.hhu.propra.uav.domains.services.UebungService;
+import de.hhu.propra.uav.domains.services.VerwaltungService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @SuppressWarnings("PMD")
 @Controller
 public class AnmeldungController {
 
     @Autowired
     private UebungService uebungService;
+    @Autowired
+    private AnmeldungService anmeldungService;
 
     @Secured("ROLE_USER")
     @GetMapping("/anmeldung")
@@ -19,4 +26,28 @@ public class AnmeldungController {
         model.addAttribute("uebungen", uebungService.findAll());
         return "anmeldung";
     }
+
+    @Secured("ROLE_USER")
+    @GetMapping("/anmeldung/{uebungId}")
+    public String anmeldungTermin(final Model model, @PathVariable("uebungId") final Long uebungId) {
+        model.addAttribute("uebung", uebungService.findById(uebungId));
+        return "anmeldungTermin";
+    }
+
+    @Secured("ROLE_USER")
+    @GetMapping("/anmeldung/{uebungId}/{terminId}")
+    public String gruppenAnmeldung(final Model model, @PathVariable("uebungId") final Long uebungId,
+                                  @PathVariable("terminId") final Long terminId) {
+        return "gruppenAnmeldung";
+    }
+
+    @Secured("ROLE_USER")
+    @PostMapping("/anmeldung/{uebungId}/{terminId}")
+    public String gruppenAnmeldungAnmelden(final Model model, @PathVariable("uebungId") final Long uebungId,
+                                  @PathVariable("terminId") final Long terminId,
+                                           final String gruppenname, final String mitglieder) {
+        anmeldungService.gruppenAnmeldung(uebungId, terminId, gruppenname, mitglieder);
+        return "gruppenAnmeldung";
+    }
+
 }
