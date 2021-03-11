e047112a1c5a1e358c348bf8d4684e710e7d17c2
YangCAO-AKM <997369876@qq.com>
2021-03-11 20:31:08 +0100

    added more Tests in StudentServiceTests.java

--- uav/infrastructure/src/test/java/de/hhu/propra/uav/domains/services/StudentServiceTests.java
+++ uav/infrastructure/src/test/java/de/hhu/propra/uav/domains/services/StudentServiceTests.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.HttpClientErrorException;
 
+import java.util.Arrays;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,12 +41,65 @@ public class StudentServiceTests {
     verify(studentRepository, times(1)).save(new Student("Alex"));
   }
 
-  @Test()
-  public void throwException() {
+  @Test
+  public void findByGithubThrowsException() {
     when(studentRepository.findByGithub(anyString())).thenReturn(Optional.empty());
 
     StudentService studentService = new StudentService(studentRepository);
-    studentService.findByGithub("Alex");
+    assertThrows(HttpClientErrorException.class,
+        () -> {
+          studentService.findByGithub("Alex");
+        });
+  }
+
+  @Test
+  public void findByIdThrowsException() {
+    when(studentRepository.findById(any())).thenReturn(Optional.empty());
+
+    StudentService studentService = new StudentService(studentRepository);
+    assertThrows(HttpClientErrorException.class,
+        () -> {
+          studentService.findById(any());
+        });
+  }
+
+  @Test
+  public void findByGithubTest() {
+    when(studentRepository.findByGithub("Alex")).thenReturn(Optional.of(new Student("Alex")));
+
+    StudentService studentService = new StudentService(studentRepository);
+
+    assertThat(studentService.findByGithub("Alex")).isEqualTo(new Student("Alex"));
+    verify(studentRepository, times(1)).findByGithub("Alex");
+  }
+
+  @Test
+  public void findByIdTest() {
+    when(studentRepository.findById(1L)).thenReturn(Optional.of(new Student("Alex")));
+
+    StudentService studentService = new StudentService(studentRepository);
+
+    assertThat(studentService.findById(1L)).isEqualTo(new Student("Alex"));
+    verify(studentRepository, times(1)).findById(1L);
+  }
+
+  @Test
+  public void findAllAsMapTest() {
+    Student alex = new Student("Alex");
+    alex.setId(1L);
+    Student bob = new Student("Bob");
+    bob.setId(2L);
+    Student charlie = new Student("Charlie");
+    charlie.setId(3L);
+
+    when(studentRepository.findAll()).thenReturn(Arrays.asList(alex,bob,charlie));
+
+    StudentService studentService = new StudentService(studentRepository);
+
+    assertThat(studentService.findAllAsMap().containsValue(alex)).isEqualTo(true);
+    assertThat(studentService.findAllAsMap().containsValue(bob)).isEqualTo(true);
+    assertThat(studentService.findAllAsMap().containsValue(charlie)).isEqualTo(true);
+    assertThat(studentService.findAllAsMap().containsValue(new Student("David"))).isEqualTo(false);
 
 
   }
