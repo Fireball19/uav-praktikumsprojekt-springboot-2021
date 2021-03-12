0d6e39bf8b2e1eeb5bba1325e781256ea3e28679
YangCAO-AKM <997369876@qq.com>
2021-03-12 20:53:12 +0100

    added AnmeldungServiceTests.java

--- uav/infrastructure/src/test/java/de/hhu/propra/uav/domains/services/AnmeldungServiceTests.java
+++ uav/infrastructure/src/test/java/de/hhu/propra/uav/domains/services/AnmeldungServiceTests.java
@@ -1,11 +1,78 @@
 package de.hhu.propra.uav.domains.services;
 
+import de.hhu.propra.uav.domains.model.uebung.Modus;
+import de.hhu.propra.uav.domains.model.uebung.Uebung;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.HttpClientErrorException;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class AnmeldungServiceTests {
 
-  @Test
-  public void gruppenAnmeldungTest() {
+  @Mock
+  UebungService uebungService;
+  @Mock
+  VerwaltungService verwaltungService;
 
+  @Test
+  public void restAnmeldungTest(){
+    AnmeldungService anmeldungService = new AnmeldungService(uebungService,verwaltungService);
+    anmeldungService.restAnmeldung(1L,2L,"Alex");
+
+    verify(verwaltungService,times(1)).addStudent("Alex",1L,2L);
+  }
+
+  @Test
+  public void gruppenAnmeldungThrowsMinException() {
+    AnmeldungService anmeldungService = new AnmeldungService(uebungService,verwaltungService);
+    Uebung testUebung = new Uebung("TestUebung", Modus.GRUPPENANMELDUNG, 2, 3,
+        LocalDateTime.now().minus(10, ChronoUnit.MINUTES),
+        LocalDateTime.now().plus(10, ChronoUnit.MINUTES));
+    when(uebungService.ueberpruefeMinGroesse(any())).thenReturn(testUebung.getMinGroesse());
+    when(uebungService.ueberpruefeMaxGroesse(any())).thenReturn(testUebung.getMaxGroesse());
+
+    assertThrows(HttpClientErrorException.class,
+        () -> {
+          anmeldungService.gruppenAnmeldung(1L,1L,
+              "TeamTest","Alex");
+        });
+  }
+
+  @Test
+  public void gruppenAnmeldungThrowsMaxException() {
+    AnmeldungService anmeldungService = new AnmeldungService(uebungService,verwaltungService);
+    Uebung testUebung = new Uebung("TestUebung", Modus.GRUPPENANMELDUNG, 2, 3,
+        LocalDateTime.now().minus(10, ChronoUnit.MINUTES),
+        LocalDateTime.now().plus(10, ChronoUnit.MINUTES));
+    when(uebungService.ueberpruefeMinGroesse(any())).thenReturn(testUebung.getMinGroesse());
+    when(uebungService.ueberpruefeMaxGroesse(any())).thenReturn(testUebung.getMaxGroesse());
+
+    assertThrows(HttpClientErrorException.class,
+        () -> {
+          anmeldungService.gruppenAnmeldung(1L,1L,
+              "TeamTest","Alex,Bob,Charlie,David");
+        });
+  }
+
+  @Test
+  public void gruppenAnmeldungThrowsNoException() {
+    AnmeldungService anmeldungService = new AnmeldungService(uebungService,verwaltungService);
+    Uebung testUebung = new Uebung("TestUebung", Modus.GRUPPENANMELDUNG, 2, 3,
+        LocalDateTime.now().minus(10, ChronoUnit.MINUTES),
+        LocalDateTime.now().plus(10, ChronoUnit.MINUTES));
+    when(uebungService.ueberpruefeMinGroesse(any())).thenReturn(testUebung.getMinGroesse());
+    when(uebungService.ueberpruefeMaxGroesse(any())).thenReturn(testUebung.getMaxGroesse());
+
+    anmeldungService.gruppenAnmeldung(1L,1L,"TeamTest","Alex,Bob,Charlie");
+
+    verify(uebungService,times(1)).addGruppe(any(),any(),any());
   }
 }
