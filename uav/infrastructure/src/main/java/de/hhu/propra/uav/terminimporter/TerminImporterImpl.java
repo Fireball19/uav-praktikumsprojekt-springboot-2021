660970dfbc0580a1d34ac86ab49e7568beef5410
Fireball19 <fireball1199@googlemail.com>
2021-03-12 20:13:56 +0100

    Added TerminImporterImpl that converts CSV files

---
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/terminimporter/TerminImporterImpl.java
@@ -0,0 +1,57 @@
+package de.hhu.propra.uav.terminimporter;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+import de.hhu.propra.uav.domains.terminimporter.TerminFile;
+import de.hhu.propra.uav.domains.terminimporter.TerminImporter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class TerminImporterImpl implements TerminImporter {
+
+  public List<TerminFile> convertToTerminFile(InputStream inputStream) {
+    List<TerminFile> termine = new ArrayList<>();
+
+    try {
+      Reader reader = new BufferedReader(new InputStreamReader(inputStream));
+      CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
+
+      String[] record;
+      while ((record = csvReader.readNext()) != null) {
+        termine.add(new TerminFile(record[0], LocalDateTime.parse(record[1],
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"))));
+      }
+
+      csvReader.close();
+      reader.close();
+
+    } catch (CsvValidationException e) {
+      new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR,"Fehler in einer CSV Zeile!");
+    } catch (IOException e) {
+      new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR,"Fehler beim einlesen der Datei!");
+    }
+
+    return termine;
+  }
+
+  public static InputStream convertMultipartFileToInputStream(MultipartFile file) {
+    InputStream inputStream = InputStream.nullInputStream();
+    try {
+      inputStream = file.getInputStream();
+    } catch (IOException e) {
+      new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR,"Fehler beim einlesen der Datei!");
+    }
+
+    return inputStream;
+  }
+}
