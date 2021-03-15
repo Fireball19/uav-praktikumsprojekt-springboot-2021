4d15c1e68221e1f3ff460d9bd753ab813c4eb5ab
zandatzu <johannes3097@web.de>
2021-03-15 13:01:24 +0100

    added Termin-Übersicht for Studenten and Tutoren

---
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/web/termin/TerminController.java
@@ -0,0 +1,37 @@
+package de.hhu.propra.uav.web.termin;
+
+import de.hhu.propra.uav.domains.model.student.Student;
+import de.hhu.propra.uav.domains.services.StudentService;
+import de.hhu.propra.uav.domains.services.UebungService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TerminController {
+
+  @Autowired
+  UebungService uebungService;
+
+  @Autowired
+  StudentService studentService;
+
+  @GetMapping("termine/uebersicht/uebungen")
+  public String tutorUebersicht(final @AuthenticationPrincipal OAuth2User principal, final Model model) {
+    if (principal.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_TUTOR"))) {
+      model.addAttribute("uebungen", uebungService.findAll());
+      return "uebungenTutoren";
+    }
+
+    String studentGithub = principal.getAttribute("login");
+    Student student = studentService.findByGithub(studentGithub);
+
+    model.addAttribute("uebungen", uebungService.findTermineByStudentId(student));
+
+    return "termineStudenten";
+  }
+}
