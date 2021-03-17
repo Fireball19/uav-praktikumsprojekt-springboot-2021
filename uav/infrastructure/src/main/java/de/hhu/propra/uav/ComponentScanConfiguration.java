b5bdb1eec6c0b3c71e46f276b6182be4236b917e
Fireball19 <fireball1199@googlemail.com>
2021-03-17 23:27:31 +0100

    Fixed configuration annotation import

--- uav/infrastructure/src/main/java/de/hhu/propra/uav/ComponentScanConfiguration.java
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/ComponentScanConfiguration.java
@@ -3,11 +3,12 @@ package de.hhu.propra.uav;
 import de.hhu.propra.uav.domains.annotations.ApplicationService;
 import de.hhu.propra.uav.domains.annotations.DomainService;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import static org.springframework.context.annotation.FilterType.ANNOTATION;
 
-@org.springframework.context.annotation.Configuration
+@Configuration
 @Profile("!test")
 @ComponentScan(
     includeFilters = {
