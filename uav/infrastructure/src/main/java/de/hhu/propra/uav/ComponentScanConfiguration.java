f03904f55b63bbaf9942cb7d02f7f4bf33e85ef6
ToxicBill <Michel144@gmx.de>
2021-03-19 13:13:04 +0100

    changed title

--- uav/infrastructure/src/main/java/de/hhu/propra/uav/ComponentScanConfiguration.java
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/ComponentScanConfiguration.java
@@ -1,12 +1,11 @@
 package de.hhu.propra.uav;
 
-import static org.springframework.context.annotation.FilterType.ANNOTATION;
-
 import de.hhu.propra.uav.domains.annotations.ApplicationService;
 import de.hhu.propra.uav.domains.annotations.DomainService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import static org.springframework.context.annotation.FilterType.ANNOTATION;
 
 @SuppressWarnings("PMD")
 @Configuration
