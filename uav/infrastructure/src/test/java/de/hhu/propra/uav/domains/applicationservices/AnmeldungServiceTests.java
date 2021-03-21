6c70d36c2bc1d04e873819beb9f9b1f96fa279b2
YangCAO-AKM <997369876@qq.com>
2021-03-21 18:02:54 +0100

    added one more Test in AnmeldungServiceTests.java

--- uav/infrastructure/src/test/java/de/hhu/propra/uav/domains/applicationservices/AnmeldungServiceTests.java
+++ uav/infrastructure/src/test/java/de/hhu/propra/uav/domains/applicationservices/AnmeldungServiceTests.java
@@ -1,19 +1,23 @@
 package de.hhu.propra.uav.domains.applicationservices;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import de.hhu.propra.uav.domains.model.uebung.Modus;
 import de.hhu.propra.uav.domains.model.uebung.Uebung;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.HttpClientErrorException;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
@@ -74,5 +78,34 @@ public class AnmeldungServiceTests {
     anmeldungService.gruppenAnmeldung(1L, 1L, "TeamTest", "Alex,Bob,Charlie");
 
     verify(uebungService, times(1)).addGruppe(any(), any(), any());
+  }
+
+  @Test
+  public void individualAnmeldungTest()
+      throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    Uebung testUebung = new Uebung("TestUebung", Modus.GRUPPENANMELDUNG, 2, 3,
+        LocalDateTime.now().minus(10, ChronoUnit.MINUTES),
+        LocalDateTime.now().plus(10, ChronoUnit.MINUTES));
+    LocalDateTime jetzt = LocalDateTime.now();
+    testUebung.addTermin("Alex",jetzt);
+    testUebung.addTermin("Bob",jetzt);
+    testUebung.addTermin("Charlie",jetzt);
+
+    Class clazz = Class.forName("de.hhu.propra.uav.domains.model.uebung.Termin");
+    Method method = clazz.getDeclaredMethod("setId", Long.class);
+    method.setAccessible(true);
+
+    method.invoke(testUebung.getTermine().get(0),1L);
+    method.invoke(testUebung.getTermine().get(1),2L);
+    method.invoke(testUebung.getTermine().get(2),3L);
+
+    AnmeldungService anmeldungService = new AnmeldungService(uebungService,verwaltungService);
+
+    when(uebungService.findById(any())).thenReturn(testUebung);
+
+    anmeldungService.individualAnmeldung(1L,jetzt,"student");
+
+    verify(verwaltungService,times(1)).addStudent("student",1L,1L);
+
   }
 }
