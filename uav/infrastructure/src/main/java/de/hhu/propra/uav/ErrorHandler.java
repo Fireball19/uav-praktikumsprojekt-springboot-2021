48933ff10be2cf76661d152142dcff98b830620a
zandatzu <johannes3097@web.de>
2021-03-22 16:22:44 +0100

    added HttpClientError-ErrorHandler

--- uav/infrastructure/src/main/java/de/hhu/propra/uav/ErrorHandler.java
+++ uav/infrastructure/src/main/java/de/hhu/propra/uav/ErrorHandler.java
@@ -3,6 +3,7 @@ package de.hhu.propra.uav;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.ModelAndView;
 
 @Profile("!test")
@@ -11,7 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class ErrorHandler {
 
   @ExceptionHandler(Exception.class)
-  public ModelAndView handleCustomRuntimeException(final Exception exception) {
+  public ModelAndView handleCustomRuntimeException() {
+    final ModelAndView mav = new ModelAndView("error");
+    mav.addObject("message", "Ein Fehler ist aufgetreten!");
+    return mav;
+  }
+
+  @ExceptionHandler(HttpClientErrorException.class)
+  public ModelAndView handleHttpClientErrorException(final Exception exception) {
     final ModelAndView mav = new ModelAndView("error");
     mav.addObject("message", exception.getMessage());
     return mav;
